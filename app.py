39398d0f2ee58e13cbd51facbb65be1a0f008f99
Freddy <ritherman03@gmail.com>
2025-09-02 18:41:39 -0500

    Parser de horas robusto (acepta HH:MM:SS, AM/PM, fracción Excel, etc.)

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ import streamlit as st
 import streamlit as st
 
 st.set_page_config(page_title="Asignador UAI", layout="wide")
-st.title("VISITA TAMBIEN")
+
 
 url_moodle = "https://moodle-admision-kkvkzem6ls2m4f458ln4ut.streamlit.app/#exportador-de-admision-moodle"
 
 st.sidebar.markdown(
+    
     f"""
+    st.title("VISITA TAMBIEN")
     <a href="{url_moodle}" target="_blank" style="text-decoration:none;">
       <span style="
         display:inline-block;
